2b307eb206e8392ea5e5d6fb71ef882d91c8b44a
Unknown <celine.bellamy@gmail.com>
2018-07-11 15:26:11 +0200

    Afficher juste les étudiants validés

--- model/entity/user_entity.php
+++ model/entity/user_entity.php
@@ -85,32 +85,6 @@ function modifUtilisateur($mot_de_passe, $email) {
     $stmt->execute();
 }
 
-function connexion(){
-    session_start();
-
-// Je vérifie si l'utilisateur est connecté
-if (isset($_SESSION['id'])) {
-    // S'il est connecté je récupère les infos de l'utilisateur en cours
-    $user = getUser($_SESSION['id']);
-} else if (isset($_POST['email']) && isset($_POST['password'])) {
-    // Utilisateur non connecté qui essai de s'identifier
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    // Est ce que l'utilisateur existe
-    $user = getUserByEmailPassword($email, $password);
-    if (isset($user['id'])) {
-        // Enregistre son identifiant dans la session
-        $_SESSION['id'] = $user['id'];
-         header("Location: ../etudiant/index.php");
-    }
-}
-
-// Si l'utilisateur n'est pas connecté, redirection vers la page de login
-if (!isset($user['id'])) {
-    header("Location: ../index.php");
-}
-}
-
 function updateUtilisateurActif(int $utilisateur_id, bool $actif){
     global $connection;
     
