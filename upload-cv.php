bd62bbef3fe39d5902c3e496a31da87fa4c7a468
Unknown <fantomette.37@hotmail.fr>
2018-07-11 09:42:47 +0200

    test upload

---
+++ upload-cv.php
@@ -0,0 +1,35 @@
+<?php
+$dossier = 'upload/';
+$fichier = basename($_FILES['cv']['name']);
+$extensions = array('.png', '.gif', '.jpg', '.jpeg', '.pdf');
+$extension = strrchr($_FILES['cv']['name'], '.'); 
+//Début des vérifications de sécurité...
+if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+{
+     $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, pdf, txt ou doc...';
+}
+if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+{
+     //On formate le nom du fichier ici...
+     $fichier = strtr($fichier, 
+          'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+          'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+     $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+     if(move_uploaded_file($_FILES['cv']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+     {
+          echo "Le fichier a bien été publié";
+     }
+     else //Sinon (la fonction renvoie FALSE).
+     {
+          echo 'Echec de l\'upload !';
+     }
+}
+else
+{
+     echo $erreur;
+}
+header('Location: test.php');
+
+
+?>
+
